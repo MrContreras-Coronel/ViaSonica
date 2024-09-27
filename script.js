5c0f70a5bc488b2dbb46e17089e2a2e1632945d5
MrContreras-Coronel <elchediassan@outlook.com>
2024-09-27 17:19:57 -0400

    Agregando la funcionalidad en JS, todavia faltan los estilos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let fu = () => {
-    
-}
+let contenedor = document.getElementById('owl')
+
+axios.get('https://api.institutoalfa.org/api/songs').then(
+    (res) => {
+          res.data.songs.map((song) => {
+
+               let songdiv = document.createElement('div')
+               songdiv.setAttribute('class','box')
+               songdiv.innerHTML = `
+                    <h3>${song.title} - ${song.author}</h3>
+               `
+
+               contenedor.appendChild(songdiv)
+          })
+    })
+
