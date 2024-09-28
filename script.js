af00982cfe2e25d4523942ef1c6603843f40499d
MrContreras-Coronel <elchediassan@outlook.com>
2024-09-28 04:01:33 -0400

    adding css styles

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let contenedor = document.getElementById('owl')
 
+const alphabetic = (a,b) => {return a.title > b.title};
+
+
 axios.get('https://api.institutoalfa.org/api/songs').then(
     (res) => {
-          res.data.songs.map((song) => {
+          res.data.songs.sort(alphabetic)
+          .map((song) => {
 
                let songdiv = document.createElement('div')
                songdiv.setAttribute('class','box')
                songdiv.innerHTML = `
+
+                    <img src="${song.image.filename}">
                     <h3>${song.title} - ${song.author}</h3>
                `
 
